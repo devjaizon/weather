6db88326b3f4c3b158d59004d94bc46c349470b7
jaizon <jaizoncarlos@protonmail.com>
2017-06-28 19:38:48 -0300

    sf

--- custom/script.js
+++ custom/script.js
@@ -1,11 +1,12 @@
 var data;
 var key = '&appid=305d9458ed8308746072a28e793b45f5';
 var unit = 'imperial';
+
 function loadData() {
     $.ajax({
         url: 'http://ip-api.com/json',
         dataType: "jsonp",
-        success: function (dt) {
+        success: function(dt) {
             country = dt.country;
             lon = '&lon=' + dt.lon;
             lat = 'lat=' + dt.lat;
@@ -13,36 +14,37 @@ function loadData() {
             $.ajax({
                 url: 'http://api.openweathermap.org/data/2.5/weather?' + lat + lon + '&units=' + unit + key,
                 dataType: "jsonp",
-                success: function (jsn) {
+                success: function(jsn) {
                     data = jsn;
                     icon = data.weather[0].icon;
                     desc = data.weather[0].description;
-                    if(unit === 'imperial') {
+                    if (unit === 'imperial') {
                         ts = '°F'
                     } else {
                         ts = '°C'
-                    $('#city-style').text(data.name+', '+region);
+                    }
+                    $('#city-style').text(data.name + ', ' + region);
                     $('#country-style').text(country);
                     $('#weather-description').text(data.weather[0].description);
                     $('#temp').html('<p>' + data.main.temp + ' ' + ts + '</p>');
                     $('.description img').attr('src', 'http://openweathermap.org/img/w/' + icon + '.png');
-                    if(desc === "few clouds") {
+                    if (desc === "few clouds") {
                         $('body').css('background-image', 'url(img/few-clouds.jpg)');
-                    } else if(desc === "clear sky") {
+                    } else if (desc === "clear sky") {
                         $('body').css('background-image', 'url(img/clear-sky.jpg)');
-                    } else if(desc === "scattered clouds") {
+                    } else if (desc === "scattered clouds") {
                         $('body').css('background-image', 'url(img/scattered-clouds.jpg)');
-                    } else if(desc === "broken clouds") {
+                    } else if (desc === "broken clouds") {
                         $('body').css('background-image', 'url(img/broken-clouds.jpg)');
-                    } else if(desc === "shower rain") {
+                    } else if (desc === "shower rain") {
                         $('body').css('background-image', 'url(img/shower-rain.jpg)');
-                    } else if(desc === "rain") {
+                    } else if (desc === "rain") {
                         $('body').css('background-image', 'url(img/rain.jpg)');
-                    } else if(desc === "thunderstorm") {
+                    } else if (desc === "thunderstorm") {
                         $('body').css('background-image', 'url(img/thunderstorm.jpg)');
-                    } else if(desc === "snow") {
+                    } else if (desc === "snow") {
                         $('body').css('background-image', 'url(img/snow.jpg)');
-                    } else if(desc === "mist") {
+                    } else if (desc === "mist") {
                         $('body').css('background-image', 'url(img/mist.jpg)');
                     } else {
                         $('body').css('background-image', 'url(img/default.jpg)');
@@ -56,7 +58,7 @@ function loadData() {
 $(document).ready(function() {
     loadData();
     $('#load').on('click', function() {
-        if(unit === 'imperial') {
+        if (unit === 'imperial') {
             unit = 'metric'
         } else {
             unit = 'imperial'
